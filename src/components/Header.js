603656119a24f56f421ba5898f6a16f33765dc22
Cassius Sanches Gachido de Souza <cassius.sanches@hotmail.com>
2019-10-16 12:26:16 -0300

    Creating Header Facebook

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src={logo} alt="Facebook" />
+        <div>
+          <span>Meu Perfil</span>
+          <i className="material-icons">account_circle</i>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
